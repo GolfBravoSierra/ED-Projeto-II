6b8e483031f5390dd2d69c4e7b3a8a180b74eaf1
GolfBravoSierra <106974254+GolfBravoSierra@users.noreply.github.com>
2023-11-16 14:54:44 -0300

    16/11/2023-QuickSort precisa ser invertido

--- ED-Projeto-II-Main.c
+++ ED-Projeto-II-Main.c
@@ -38,12 +38,13 @@ void troca(no *a, no *b){
     *a = *b;
     *b = aux;
 }
+// ---------------------------------------------------------------------
 
-//InsertionSort function lower to higher
-double insertionSort(no *vetor, int tamanho){
+//InsertionSort function lower to higher ---------
+void insertionSort(no *vetor, int tamanho){
 
     //parte para da biblioteca time.h para marcar o tempo de execução
-    clock_t start, end;
+    clock_t start = 0, end = 0;
     double cpu_time_used;
 
     start = clock();
@@ -63,47 +64,47 @@ double insertionSort(no *vetor, int tamanho){
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
     printf("\n\nTempo de execucao(insertsort): %f\n", cpu_time_used);
-    printf("tempo de inicio %ld\n", start);
-    return cpu_time_used;
+    printf("primeia posicao %d\n", vetor[0].chave);
+    printf("ultima posicao %d\n", vetor[tamanho-1].chave);
 }
+//--------------------------------------------
 
-
-//BubleSort function lower to higher
-double bubleSort(no *vetor, int tamanho){
+//BubleSort function lower to higher -------------------------------
+void bubleSort(no *vetor, int tamanho){
 
     //parte para da biblioteca time.h para marcar o tempo de execução
-    clock_t start, end;
+    clock_t start = 0, end = 0;
     double cpu_time_used;
 
     start = clock();
 
-    int i = 0 , j = 0 ;
-    no aux;
-    for(i = 0 ; i < tamanho - 1; i--)
+    int i, j;
+    for(i = 0 ; i < tamanho - 1; i++)
     {
-        for (j = 1 ; j  < tamanho - i; j--)
+        for (j = 1 ; j  < tamanho - i; j++)
         {
             if(vetor[j].chave > vetor[j-1].chave)
             {
-                aux = vetor[j];
-                vetor[j] = vetor[j+1];
-                vetor[j-1] = aux;
+                troca(&vetor[j-1], &vetor[j]);
             }
         }
     }
 
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    return cpu_time_used;
+    printf("\n\nTempo de execucao(bulbesort): %f\n", cpu_time_used);
+    printf("primeia posicao %d\n", vetor[0].chave);
+    printf("ultima posicao %d\n", vetor[tamanho-1].chave);
 }
+//------------------------------------------------------------------
 
-//Shell Sort function lower to higher
-double shellsort(no *vetor , int tamanho){
+//Shell Sort function lower to higher -------------------------------
+void shellsort(no *vetor , int tamanho){
     int i = 0  , j = 0 , h;
     no aux;
 
     //parte para da biblioteca time.h para marcar o tempo de execução
-    clock_t start, end;
+    clock_t start = 0, end = 0;
     double cpu_time_used;
 
     start = clock();
@@ -119,7 +120,7 @@ double shellsort(no *vetor , int tamanho){
             while(vetor[j-h].chave < aux.chave)
             {
                 vetor[j] = vetor[j-h];
-                j = j-h;
+                j = h;
                 if(j < h)
                 {
                     break;
@@ -131,11 +132,16 @@ double shellsort(no *vetor , int tamanho){
 
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    return cpu_time_used;
+    printf("\n\nTempo de execucao(shellsort): %f\n", cpu_time_used);
+    printf("primeia posicao %d\n", vetor[0].chave);
+    printf("ultima posicao %d\n", vetor[tamanho-1].chave);
 }
-/*
-//MergeSort function lower to higher
-void merger(int *v , int *c, int i, int m , int f){
+//------------------------------------------------------------------
+
+//MergeSort function lower to higher-----------
+
+
+void merge(no *v , no *c, int i, int m , int f){
     int z, iv = i, ic = m+1;
     for(z = i; z <= f; z++)
     {
@@ -144,106 +150,161 @@ void merger(int *v , int *c, int i, int m , int f){
     z = i;
     while (iv <= m && ic <= f)
     {
-        if (c[iv] <= c[ic])
+        if (c[iv].chave <= c[ic].chave)
         {
-            v[z] = c[iv++];
+            v[z++] = c[iv++];
         }
         else
         {
-            v[z] = c[ic++];
+            v[z++] = c[ic++];
         }
     }
     while (iv <= m)
     {
-    v[z++] = c[iv++];
+        v[z++] = c[iv++];
     }
     while (ic <= f)
     {
-    v[z++] = c[ic++];
+        v[z++] = c[ic++];
     }
 }
 
-void sort (no *v, int *c, int i,int f){
+void sort(no *v, no *c, int i,int f){
     if (i < f)
     {
         int m = (i+f)/2;
         sort(v, c, i, m);
         sort(v, c, m+1, f);
-        if (v.chave[m] > v[m+1])
+        if (v[m].chave < v[m+1].chave)
         {
-            merger(v, c, i, m, f);
+            merge(v, c, i, m, f);
         }
     }
 }
 
-double mergesort (no *v , int n){
+void mergesort(no *v , int n){
 
     //parte para da biblioteca time.h para marcar o tempo de execução
     clock_t start = 0, end;
     double cpu_time_used;
 
-    int *c = malloc(sizeof(int)*n);
+    //parte da função merge sort
+    no *c = malloc(sizeof(no)*n);
     sort (v, c, 0, n-1);
     free(c);
 
+    //parte para da biblioteca time.h para marcar o tempo de execução e verificar a ordenação
     end = clock();
     cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-    return cpu_time_used;
+    printf("\n\nTempo de execucao(mergesort): %f\n", cpu_time_used);
+    printf("primeia posicao %d\n", v[0].chave);
+    printf("ultima posicao %d\n", v[n-1].chave);
+
+}
+//---------------------------------------------
+
+//QuickSort function lower to higher-----------
+
+int particao(no *v, int LI, int LS){
+    int pivo, aux;
+    int e = LI, d = LS;
+    pivo = v[LI].chave;
+    while(e < d)
+    {
+        while((v[e].chave >= pivo)&&(e < LS))
+        {
+            e++;
+        }
+        while((v[d].chave > pivo)&&(d > LI))
+        {
+            d--;
+        }
+        if(e < d)
+        {
+            aux = v[e].chave;
+            v[e].chave = v[d].chave;
+            v[d].chave = aux;
+        }
+    }
+    aux = v[LI].chave;
+    v[LI].chave = v[d].chave;
+    v[d].chave = aux;
+    return d;
 }
 
-*/
+double quicksort(no *v ,int LI,  int LS ){
 
+    // patente para da biblioteca time.h para marcar o tempo de execução
+    clock_t start = 0, end;
+    double cpu_time_used;
 
+    if(LI<LS)
+    {
+        int p;
+        p= particao(v,LI,LS);
+        quicksort(v,LI,p-1);
+        quicksort(v,p+1,LS);
+    }
+
+    //parte para da biblioteca time.h para marcar o tempo de execução e verificar a ordenação
+    end = clock();
+    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("primeia posicao %d\n", v[0].chave);
+    printf("ultima posicao %d\n", v[LS-1].chave);
+    return cpu_time_used;
+    
+}
+
+//---------------------------------------------
 int main(){
     int size = 10; // tamanho do vetor
     int seed = 0; // seed para geraçõa do vetor randomico
-    double time = 0; // variavel para armazenar o tempo de execução
+    
     no* vector = (no *)malloc(size * sizeof(no));
 
 //-------------------------VetorDesordenado(CASO-1)--------------------------------------
     seed = 22007263; // seed para geraçõa do vetor randomico
     createRandomVector(vector, size, seed);
-
     //para imprimir o vetor desordenado descomente a linha abaixo
     //printf("Vetor desordenado:\n"); imprimevetor(vector, size);
 //--------------------------------------------------------------------------------
 
 //-------------------------VetorOrdenadoInsertSort-----------------------------------------
-    time = insertionSort(vector, size);
+    insertionSort(vector, size);
 
     //para imprimir o vetor ordenado pelo insertSort descomente a linha abaixo
     //printf("\nVetor ordenadoinsetsort:\n"); imprimevetor(vector, size);
-
-    printf("primeia posicao %d\n", vector[0].chave);
-    printf("\n\nTempo de execucao(insertsort): %f\n", time);
 //-----------------------------------------------------------------------------------------
 
-//-------------------------VetorOrdenadoBubleSort-----------------------------------------
-    time = bubleSort(vector, size);
+//-------------------------VetorOrdenadoBubleSort-----------------------------------------  
+    bubleSort(vector, size);
 
     //para imprimir o vetor ordenado pelo bubleSort descomente a linha abaixo
-    printf("\nVetor ordenado(bublesort):\n"); imprimevetor(vector, size);
-
-    printf("\n\nTempo de execucao(bulbesort): %f\n", time);
+    //printf("\nVetor ordenado(bublesort):\n"); imprimevetor(vector, size);
 //-----------------------------------------------------------------------------------------
 
-
-//-------------------------VetorOrdenadoShellSort-----------------------------------------
-    time = shellsort(vector, size);
+//-------------------------VetorOrdenadoShellSort-----------------------------------------  
+    shellsort(vector, size);
 
     //para imprimir o vetor ordenado pelo shellSort descomente a linha abaixo
     //printf("\nVetor ordenado(shellsort):\n"); imprimevetor(vector, size);
-
-    printf("\n\nTempo de execucao(shellsort): %f\n", time);
 //-----------------------------------------------------------------------------------------
 
 //-----------------------MergeSort---------------------------------------------------------
-    //time = mergesort(vector , size);
-
+    mergesort(vector , size);
+    
     //para imprimir o vetor ordenado pelo mergeSort descomente a linha abaixo
     //printf("\nVetor ordenado(mergesort):\n"); imprimevetor(vector, size);
+//-----------------------------------------------------------------------------------------
 
-    printf("\n\nTempo de execucao(mergesort): %f\n", time);
+//-----------------------QuickSort---------------------------------------------------------
+    double tempo;
+    tempo = quicksort(vector , 0 , size);
+    printf("\n\nTempo de execucao(quicksort): %f\n", tempo);
+
+
+    //para imprimir o vetor ordenado pelo quickSort descomente a linha abaixo
+    printf("\nVetor ordenado(quicksort):\n"); imprimevetor(vector, size);
 //-----------------------------------------------------------------------------------------
 
     free(vector);
